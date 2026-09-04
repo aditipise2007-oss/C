fd4b55957b772a216f5c6b1ea8527fab784c0624
Aditi <aditipise2007@gmail.com>
2026-09-04 23:50:33 +0530

    fun16 is added

---
+++ fun16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int reverse(int a);
+int main() {
+    int a,rev;
+    printf("Enter the number:");
+    scanf("%d", &a);
+
+    if(a == reverse(a))
+        {
+         printf("palindrom number");
+    }
+    else
+    {  
+        printf("not palindrom");            
+    }
+    return 0;
+}
+int check(int a){
+    int rev=0,rem;
+
+    while(a!=0)
+    {
+        rem=a%10;
+        rev=rev*10+rem;
+        a=a/10;
+
+    }
+
+return rev;
+}
