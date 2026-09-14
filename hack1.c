a39a72165a4f61aa60262a61adf39738bc739319
Aditi <aditipise2007@gmail.com>
2026-09-14 09:21:34 +0530

    hack1 is added

---
+++ hack1.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+int main()
+{
+int a, b,ch;
+    scanf("%d\n%d", &a, &b);
+  	 printf("&d \n &d", a, b);
+
+switch(ch)
+{
+case1:printf("one=%d", 1);
+break;
+
+case2:printf("two=%d", 2);
+break;
+
+case3:printf("three=%d", 3);
+break;
+
+case4:printf("four=%d", 4);
+break;
+
+case5:printf("five=%d", 5);
+break;
+
+case6:printf("six=%d", 6);
+break;
+
+case7:printf("seven=%d", 7);
+break;
+
+case8:printf("eight=%d", 8);
+break;
+
+case9:printf("nine=%d", 9);
+break;
+
+}
+
+if(a%2==0)
+{
+printf("even");
+}
+else
+{
+printf(odd);
+}
+if(b%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+return 0;
+}
